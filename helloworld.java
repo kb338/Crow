d5c882180c5ecab87d0547d0dc800f50ac028e02
kb338 <kb338825@gmail.com>
2020-07-17 12:11:07 +0530

    first java program

---
+++ helloworld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+     public static void main(string[] args) {
+     //prints "Hello, World" to the trminal window.
+     System.out.println("Hello, World");
+     }
+     
+     }
